67172e52f976dc98239603b80a139e00c81007bd
Kumarguru Viswanath <36629432+77CaptainClaw77@users.noreply.github.com>
2023-07-10 20:17:09 +0530

    Fixed bug with button to modal mapping.  (#2)
    * Version 2.1
    
    Simplified and rewrote the script.js
    
    * Version 2.2
    
    Fixed a bug with button to modal mapping.
    
    ---------
    
    Co-authored-by: ARUN P N <52566445+pnarun@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -1,16 +1,30 @@
-var modals = ["sslcModal", "pucModal", "engModal", "corpModal"]
-var btns = ["sslcbtn", "pucbtn", "engbtn", "corpbtn"]
-var spans = ["sslcclose", "pucclose", "engclose", "corpclose"]
-
+var modals = ["sslcModal", "pucModal", "engModal", "corpModal"];
+var btns = ["sslcbtn", "pucbtn", "engbtn", "corpbtn"];
+var spans = ["sslcclose", "pucclose", "engclose", "corpclose"];
+var btn_modal = {};
+var span_modal = {};
 for (var i = 0; i < 4; i++) {
-  var modal = document.getElementById(modals[i]);
-  var btn   = document.getElementById(btns[i]);
-  var span  = document.getElementsByClassName(spans[i])[0];
-  btn.onclick = function () {
-    modal.style.display = "block";
+  btn_modal[btns[i]] = modals[i];
+  span_modal[spans[i]] = modals[i];
+  document.getElementById(btns[i]).onclick = function () {
+    document.getElementById(btn_modal[this.id]).style.display = "block";
   }
-  span.onclick = function () {
-    modal.style.display = "none";
+  document.getElementsByClassName(spans[i])[0].onclick = function () {
+    document.getElementById(span_modal[this.className]).style.display = "none";
+
+// var modals = ["sslcModal", "pucModal", "engModal", "corpModal"]
+// var btns = ["sslcbtn", "pucbtn", "engbtn", "corpbtn"]
+// var spans = ["sslcclose", "pucclose", "engclose", "corpclose"]
+
+// for (var i = 0; i < 4; i++) {
+//   var modal = document.getElementById(modals[i]);
+//   var btn   = document.getElementById(btns[i]);
+//   var span  = document.getElementsByClassName(spans[i])[0];
+//   btn.onclick = function () {
+//     modal.style.display = "block";
+//   }
+//   span.onclick = function () {
+//     modal.style.display = "none";
   }
 }
 
